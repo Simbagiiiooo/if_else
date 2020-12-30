29235e14dc1b537b57f3ac66ee406f99f61142d9
Simbagiiiooo <67731418+Simbagiiiooo@users.noreply.github.com>
2020-12-30 15:16:06 +0100

    Add files via upload

---
+++ 21_if_else/script.js
@@ -0,0 +1,18 @@
+let age = 17;
+let isFemale = false;
+let driverStatus = 'Hans';
+if (driverStatus =='Bob') {
+    console.log("Jij mag autorijden!");
+} else {
+    console.log("Gij mag nie rijen, gij het gezopen!!");
+}
+if (isFemale) {
+    console.log("Hoeveel kaartjes wilt u?");
+} else {
+    console.log("Een vrouw met een piel, dacht het niet!!");
+}
+if (age >= 18) {
+    console.log("Kom maar binnen!");
+} else {
+    console.log("Jij bent jonger dan 18 Fuck off!!!");
+}
